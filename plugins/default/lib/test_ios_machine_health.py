bb2faa1ad602b7882d7e30eaa249beb6ae7be974
Laura Fitzgerald <lfitzger@redhat.com>
2017-07-25 09:45:40 +0100

    ios_check_tests

--- plugins/default/lib/test_ios_machine_health.py
+++ plugins/default/lib/test_ios_machine_health.py
@@ -1,7 +1,13 @@
 import unittest
 import nagios
 
-from ios_machine_health import report
+from ios_machine_health import report, parse_response, check
+
+
+class TestCheck(unittest.TestCase):
+
+    def runTest(self):
+        self.assertRaises(ValueError, check, "", "", "", "")
 
 
 class TestReport(unittest.TestCase):
@@ -9,30 +15,61 @@ class TestReport(unittest.TestCase):
     def runTest(self):
         self.assertEqual(report(
             results=[
-                ['machine1', nagios.OK],
-                ['machine2', nagios.OK]
+                ["macOS (machine1)", nagios.OK],
+                ["macOS (machine2)", nagios.OK]
             ],
             errors=(),
             ), nagios.OK)
         self.assertEqual(report(
             results=[
-                ['machine1', nagios.OK],
-                ['machine2', nagios.CRIT]
+                ["macOS (machine1)", nagios.OK],
+                ["macOS (machine2)", nagios.CRIT]
             ],
             errors=()
             ), nagios.CRIT)
         self.assertEqual(report(
             results=[
-                ['machine1', nagios.OK],
-                ['machine2', nagios.CRIT],
-                ['machine3', nagios.UNKNOWN]
+                ["macOS (machine1)", nagios.OK],
+                ["macOS (machine2)", nagios.CRIT],
+                ["macOS (machine3)", nagios.UNKNOWN]
             ],
             errors=()
             ), nagios.UNKNOWN)
         self.assertEqual(report(
             results=[],
-            errors=['machine1', nagios.UNKNOWN]
+            errors=["machine1", nagios.UNKNOWN]
             ), nagios.UNKNOWN)
+
+
+class TestParseResponse(unittest.TestCase):
+
+    def runTest(self):
+        self.assertEqual(parse_response(
+                    response={"displayName": "Nodes", "busyExecutors": 0, "computer": [
+                            {
+                                "displayName": "master", "offline": False
+                            },
+                            {
+                                "displayName": "macOS machine1", "offline": False
+                            }
+                        ]
+                    }),
+            ([('macOS machine1', nagios.OK)], [])
+            )
+        self.assertEqual(parse_response(
+                    response={"displayName": "Nodes", "busyExecutors": 0, "computer": [
+                            {
+                                "displayName": "master", "offline": True
+                            },
+                            {
+                                "displayName": "macOS machine1", "offline": True
+                            }
+                        ]
+                    }),
+            ([("macOS machine1", nagios.CRIT)], [])
+            )
+        self.assertRaises(KeyError, parse_response,
+                          response={"msg": "some other response from the server"})
 
 
 if __name__ == "__main__":
